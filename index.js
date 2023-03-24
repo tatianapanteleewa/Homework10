6cc0f3f62eb6d15c93c3944b00a45807bef656ae
tatianapanteleewa <tanyushka5812.efimova@yandex.ru>
2023-03-24 12:02:35 +0300

    new commit

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const commentInputElement = document.getElementById('comment-input');
 const likeCounter = document.querySelectorAll('.likes-counter');
 const inputText = document.getElementById("comment-input");
 
-
-
 inputText.value = "";
-
-
-
 let comments = [];
 
 fetch(
@@ -133,36 +128,35 @@ buttonElement.addEventListener('click', () => {
         }),
     }).then((response) => {
         response.json().then((responseData) => {
-            comments = responseData.comments;
-            renderComments();
+            fetch(
+                'https://webdev-hw-api.vercel.app/api/v1/:Panteleewa-Tatiana/comments', {
+                method: "GET"
+            }).then((response) => {
+                response.json().then((responseData) => {
+                    const options = {
+                        year: '2-digit',
+                        month: 'numeric',
+                        day: 'numeric',
+                        timezone: 'UTC',
+                        hour: 'numeric',
+                        minute: '2-digit',
+                    };
+                    const appComments = responseData.comments.map((comment) => {
+                        return {
+                            name: comment.author.name,
+                            date: comment.date.toLocaleString('ru-RU', options),
+                            text: comment.text,
+                            likes: comment.likes,
+                            isLiked: false,
+                        };
+                    });
+                    comments = appComments;
+                    renderComments();
+                })
+            });
         })
     })
-    fetch(
-        'https://webdev-hw-api.vercel.app/api/v1/:Panteleewa-Tatiana/comments', {
-        method: "GET"
-    }).then((response) => {
-        response.json().then((responseData) => {
-            const options = {
-                year: '2-digit',
-                month: 'numeric',
-                day: 'numeric',
-                timezone: 'UTC',
-                hour: 'numeric',
-                minute: '2-digit',
-            };
-            const appComments = responseData.comments.map((comment) => {
-                return {
-                    name: comment.author.name,
-                    date: comment.date.toLocaleString('ru-RU', options),
-                    text: comment.text,
-                    likes: comment.likes,
-                    isLiked: false,
-                };
-            });
-            comments = appComments;
-            renderComments();
-        })
-    });
+
     nameInputElement.value = '';
     commentInputElement.value = '';
 

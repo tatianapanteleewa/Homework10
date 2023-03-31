9a7a8b40514bf7b80849d4e306947ecdbf9a9b5e
tatianapanteleewa <tanyushka5812.efimova@yandex.ru>
2023-03-31 13:13:54 +0300

    modul

---
+++ fetchAndRenderComments.js
@@ -0,0 +1,35 @@
+let comments = [];
+
+const fetchAndRenderComments = () => {
+
+    return fetch(
+        'https://webdev-hw-api.vercel.app/api/v1/:Panteleewa-Tatiana/comments', {
+        method: "GET"
+    }).then((response) => {
+        return response.json();
+    })
+        .then((responseData) => {
+            const options = {
+                year: '2-digit',
+                month: 'numeric',
+                day: 'numeric',
+                timezone: 'UTC',
+                hour: 'numeric',
+                minute: '2-digit',
+            };
+
+            const appComments = responseData.comments.map((comment) => {
+                return {
+                    name: comment.author.name,
+                    date: new Date(comment.date).toLocaleString("ru-RU", options),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false,
+                };
+            });
+            comments = appComments;
+            renderComments();
+        });
+};
+
+export { comments };
--- index.js
+++ index.js
@@ -1,114 +1,151 @@
+import { comments } from "./fetchAndRenderComments";
+import { getListCommentsEdit } from "./getListComments";
+import renderComments from "./renderComments";
+
 const buttonElement = document.getElementById('add-button');
 const listElement = document.getElementById('list');
 const nameInputElement = document.getElementById('name-input');
 const commentInputElement = document.getElementById('comment-input');
-const likeCounter = document.querySelectorAll('.likes-counter');
-const inputText = document.getElementById("comment-input");
+// const likeCounter = document.querySelectorAll('.likes-counter');
+// const inputText = document.getElementById("comment-input");
 
 commentInputElement.value = "";
 nameInputElement.value = "";
 
-let comments = [];
+// let comments = [];
 
 const downloadAlert = () => {
     alert('Приложение загружается...');
 }
 
-const fetchAndRenderComments = () => {
-    return fetch(
-        'https://webdev-hw-api.vercel.app/api/v1/:Panteleewa-Tatiana/comments', {
-        method: "GET"
-    }).then((response) => {
-        return response.json();
-    })
-        .then((responseData) => {
-            const options = {
-                year: '2-digit',
-                month: 'numeric',
-                day: 'numeric',
-                timezone: 'UTC',
-                hour: 'numeric',
-                minute: '2-digit',
-            };
+// const fetchAndRenderComments = () => {
+//     return fetch(
+//         'https://webdev-hw-api.vercel.app/api/v1/:Panteleewa-Tatiana/comments', {
+//         method: "GET"
+//     }).then((response) => {
+//         return response.json();
+//     })
+//         .then((responseData) => {
+//             const options = {
+//                 year: '2-digit',
+//                 month: 'numeric',
+//                 day: 'numeric',
+//                 timezone: 'UTC',
+//                 hour: 'numeric',
+//                 minute: '2-digit',
+//             };
 
-            const appComments = responseData.comments.map((comment) => {
-                return {
-                    name: comment.author.name,
-                    date: new Date(comment.date).toLocaleString("ru-RU", options),
-                    text: comment.text,
-                    likes: comment.likes,
-                    isLiked: false,
-                };
-            });
-            comments = appComments;
-            renderComments();
-        });
-};
+//             const appComments = responseData.comments.map((comment) => {
+//                 return {
+//                     name: comment.author.name,
+//                     date: new Date(comment.date).toLocaleString("ru-RU", options),
+//                     text: comment.text,
+//                     likes: comment.likes,
+//                     isLiked: false,
+//                 };
+//             });
+//             comments = appComments;
+//             renderComments();
+//         });
+// };
 
-const clickLikeButton = () => {
-    const likeButtonsElements = document.querySelectorAll('.like-button');
-    for (const likeButtonElement of likeButtonsElements) {
-        likeButtonElement.addEventListener('click', (event) => {
-            const index = likeButtonElement.dataset.index;
-            if (comments[index].isLiked === false) {
-                comments[index].isLiked = true;
-                comments[index].likes += 1;
-            } else if (comments[index].isLiked === true) {
-                comments[index].isLiked = false;
-                comments[index].likes -= 1;
-            }
-            renderComments();
-            event.stopPropagation();
+// const clickLikeButton = () => {
+//     const likeButtonsElements = document.querySelectorAll('.like-button');
+//     for (const likeButtonElement of likeButtonsElements) {
+//         likeButtonElement.addEventListener('click', (event) => {
+//             const index = likeButtonElement.dataset.index;
+//             if (comments[index].isLiked === false) {
+//                 comments[index].isLiked = true;
+//                 comments[index].likes += 1;
+//             } else if (comments[index].isLiked === true) {
+//                 comments[index].isLiked = false;
+//                 comments[index].likes -= 1;
+//             }
+//             renderComments();
+//             event.stopPropagation();
 
-        });
-    }
-};
+//         });
+//     }
+// };
 
-const clickComment = () => {
-    const commentElements = document.querySelectorAll(".comment");
-    for (const commentElement of commentElements) {
-        commentElement.addEventListener("click", () => {
-            const index = commentElement.dataset.index;
-            const userComment = `>  ${comments[index].text} \n \n ${comments[index].name}`;
-            document.getElementById("comment-input").value = userComment;
-            console.log(userComment);
+// const clickComment = () => {
+//     const commentElements = document.querySelectorAll(".comment");
+//     for (const commentElement of commentElements) {
+//         commentElement.addEventListener("click", () => {
+//             const index = commentElement.dataset.index;
+//             const userComment = `>  ${comments[index].text} \n \n ${comments[index].name}`;
+//             document.getElementById("comment-input").value = userComment;
+//             console.log(userComment);
 
-        })
-    }
-}
+//         })
+//     }
+// }
 
-const renderComments = () => {
-    const commentsHtml = comments
-        .map((comment, index) => {
-            return `<li id = "list-comment" data-index = '${index}' class="comment">
-          <div class="comment-header">
-            <div>${comment.name}</div>
-            <div>${comment.date}</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">
-              ${comment.text}
-            </div>
-          </div>
-          <div class="comment-footer">
-            <div class="likes">
-              <span class="likes-counter">${comment.likes}</span>
-              <button data-index = '${index}' class="${comment.liked ? 'like-button -active-like' : 'like-button'
-                }"></button>
-            </div>
-          </div>
-        </li>`;
-        })
-        .join('');
+// const renderComments = () => {
+//     const commentsHtml = comments
+//         .map((comment, index) => {
+//             return `<li id = "list-comment" data-index = '${index}' class="comment">
+//           <div class="comment-header">
+//             <div>${comment.name}</div>
+//             <div>${comment.date}</div>
+//           </div>
+//           <div class="comment-body">
+//             <div class="comment-text">
+//               ${comment.text}
+//             </div>
+//           </div>
+//           <div class="comment-footer">
+//             <div class="likes">
+//               <span class="likes-counter">${comment.likes}</span>
+//               <button data-index = '${index}' class="${comment.liked ? 'like-button -active-like' : 'like-button'
+//                 }"></button>
+//             </div>
+//           </div>
+//         </li>`;
+//         })
+//         .join('');
 
-    listElement.innerHTML = commentsHtml;
-    clickLikeButton();
-    clickComment();
-};
+//     listElement.innerHTML = commentsHtml;
+//     clickLikeButton();
+//     clickComment();
+// };
+
 
 downloadAlert();
 fetchAndRenderComments();
-renderComments();
+
+// const getListCommentsEdit = (comment, index) => {
+//     return `<li id = "list-comment" data-index = '${index}' class="comment">
+//           <div class="comment-header">
+//             <div>${comment.name}</div>
+//             <div>${comment.date}</div>
+//           </div>
+//           <div class="comment-body">
+//             <div class="comment-text">
+//               ${comment.text}
+//             </div>
+//           </div>
+//           <div class="comment-footer">
+//             <div class="likes">
+//               <span class="likes-counter">${comment.likes}</span>
+//               <button data-index = '${index}' class="${comment.liked ? 'like-button -active-like' : 'like-button'
+//         }"></button>
+//             </div>
+//           </div>
+//         </li>`;
+// };
+
+
+// const renderComments = (getListComments) => {
+//     const commentsHtml = comments
+//         .map((comment, index) => getListComments(comment, index)).join("");
+
+//     listElement.innerHTML = commentsHtml;
+//     clickLikeButton();
+//     clickComment();
+// };
+
+renderComments(listElement, getListCommentsEdit);
 
 buttonElement.addEventListener('click', () => {
 
@@ -148,10 +185,7 @@ buttonElement.addEventListener('click', () => {
                 throw new Error('Сервер упал');
             }
         })
-        .then(() => {
-            return fetchAndRenderComments();
-
-        })
+        .then(fetchAndRenderComments)
         .then((data) => {
             buttonElement.disabled = false;
             buttonElement.textContent = 'Написать';
@@ -164,6 +198,8 @@ buttonElement.addEventListener('click', () => {
             alert('Кажется, что-то пошло не так, попробуйте позже...');
             console.warn(error);
         });
+
+    renderStudents(listElement, getListStudentsEdit);
 });
 
 
